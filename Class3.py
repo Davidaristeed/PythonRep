a0c9f2ba0856a67c33451e08a669a7a50167419b
davidaristeed <davidaristeed@gmail.com>
2022-01-24 10:24:20 +0300

    2nd class

---
+++ Class3.py
@@ -0,0 +1,40 @@
+a = 10;
+b = 20.135;
+c = "Hello world";
+d = True ;
+print(a)
+print(b)
+print(c)
+
+# Operators
+x = a + b;
+print(x)
+
+y =  a * 2 ;
+print (y)
+
+z = b % 3
+print(z)
+
+# Checking data types
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#TYPE CONVERSION
+age = input("Enter age")
+print(age)
+print(type(age))
+
+r = "python"
+
+# This is a single line comment
+"""This is 
+    a multiline
+    comment.
+    It goes even here"""
+    
+print(a + b)
+
+# print (c + a )
